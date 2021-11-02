0ae0590e1630cbdcf6434d80103475e95bac32e9
overhex <46409526+overhex@users.noreply.github.com>
2021-11-02 22:06:46 +0300

    Update umee1c_install.sh
    fix

--- umee1c_install.sh
+++ umee1c_install.sh
@@ -18,7 +18,7 @@ echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
 
 echo 'export GO111MODULE=on' >> $HOME/.bash_profile
 
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile &&
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
 
 source $HOME/.bash_profile
 
