7a159e23da118458062f8732ba7b042cb877eb82
overhex <46409526+overhex@users.noreply.github.com>
2021-11-21 04:12:29 +0300

    Update umee1c_install.sh
    fix

--- umee1c_install.sh
+++ umee1c_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '✨This will install umee node on umeevengers-1c chain✨'
-echo '🌌Provide you node name and wait for installation ends🌌'
+echo 'This will install umee node on umeevengers-1c chain✨'
+echo 'Provide you node name and wait for installation ends🌌'
 echo ''
 echo 'Umee node name:'
 read UMEE_NODENAME
