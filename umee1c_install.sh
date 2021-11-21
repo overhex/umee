4446a99a5b26b33e340ec280adeb1660f29f5966
overhex <46409526+overhex@users.noreply.github.com>
2021-11-21 04:02:58 +0300

    Update umee1c_install.sh
    fix

--- umee1c_install.sh
+++ umee1c_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo 'This will install umee node in umeevengers-1c chain'
+echo 'This will install umee node on umeevengers-1c chain'
 echo 'Provide you node name and wait for installation ends'
 echo ''
-read -p "Umee node name: " UMEE_NODENAME
+echo 'Umee node name:'
+read UMEE_NODENAME
 echo 'export UMEE_NODENAME='\"${UMEE_NODENAME}\" >> $HOME/.bash_profile
 
 sudo apt update
@@ -31,6 +32,18 @@ umeed init ${UMEE_NODENAME} --chain-id=umeevengers-1c
 rm $HOME/.umee/config/genesis.json
 wget -O $HOME/.umee/config/genesis.json "https://raw.githubusercontent.com/umee-network/testnets/main/networks/umeevengers-1c/genesis.json"
 
+umeed unsafe-reset-all
+
+wget -O $HOME/.umee/config/addrbook.json "https://raw.githubusercontent.com/overhex/umee/main/addrbook-1c.json"
+
+NODE_IP=`curl ifconfig.me`  
+
+echo 'Your node IP: '$NODE_IP
+
+PEERS="1694e2cd89b03270577e547d7d84ebef13e4eff1@172.105.168.226:26656,4d50abb293f399a0f41ef9dbebe62615d4c85e42@3.34.147.65:26656,d2447c2ba201fb5bdd7250921c7c267af18c0950@94.130.23.149:26656,901a625ecf43014cc383239524c5eb6595a56888@135.181.165.110:26656,4ea1dc6af45f0fad7315029d181ada53f7d3174c@161.97.182.71:26656,60a11b328f161fe8f3f98f85e838addb07513c9e@46.101.234.47:26656,4bf9ff17d148418aec04fdda9bff671e482457a3@213.202.252.173:26656,1fb83420fd2bf665dc886fb3727d809579d63e51@206.189.133.102:26656,b85598b96a9c8e835b7b2f2c0b322eb2317fe7cd@94.250.201.70:26656"
+
+sed -i.bak -e "s/^NODE_IP = \"\"/NODE_IP = \"$NODE_IP:26656\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.umee/config/config.toml
+
 cd $HOME
 
 echo "[Unit]
@@ -49,23 +62,6 @@ WantedBy=multi-user.target" > $HOME/umeed.service
 
 sudo mv $HOME/umeed.service /etc/systemd/system
 
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-
-umeed unsafe-reset-all
-
-wget -O $HOME/.umee/config/addrbook.json "https://raw.githubusercontent.com/overhex/umee/main/addrbook-1c.json"
-
-nodeIP=`curl ifconfig.me`  
-
-echo 'Your node IP: '$nodeIP
-
-peers="1694e2cd89b03270577e547d7d84ebef13e4eff1@172.105.168.226:26656,4d50abb293f399a0f41ef9dbebe62615d4c85e42@3.34.147.65:26656,d2447c2ba201fb5bdd7250921c7c267af18c0950@94.130.23.149:26656,901a625ecf43014cc383239524c5eb6595a56888@135.181.165.110:26656,4ea1dc6af45f0fad7315029d181ada53f7d3174c@161.97.182.71:26656,60a11b328f161fe8f3f98f85e838addb07513c9e@46.101.234.47:26656,4bf9ff17d148418aec04fdda9bff671e482457a3@213.202.252.173:26656,1fb83420fd2bf665dc886fb3727d809579d63e51@206.189.133.102:26656,b85598b96a9c8e835b7b2f2c0b322eb2317fe7cd@94.250.201.70:26656"
-
-sed -i.bak -e "s/^nodeIP = \"\"/nodeIP = \"$nodeIP:26656\"/; s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.umee/config/config.toml
-
-sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
 sudo systemctl enable umeed
 sudo systemctl restart umeed
